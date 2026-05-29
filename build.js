31a159a661bb40ac545555aa696f70849f970115
Aayu Parikh <aryanparikh03@gmail.com>
2026-05-30 00:54:36 +0530

    Restructure repository: Move Next.js app to root and remove old static HTML files

--- build.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const source = path.join(__dirname, 'home.html');
-const dest = path.join(__dirname, 'index.html');
-
-fs.copyFileSync(source, dest);
-console.log('Built index.html from home.html');
